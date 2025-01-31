ec7b32d5bc737c8c3dfd9e6ab2701041f5994e42
Vritant Bhardwaj <vritoku@gmail.com>
2025-01-31 14:23:32 -0800

    reduce text

--- src/web/extension.ts
+++ src/web/extension.ts
@@ -22,17 +22,14 @@ export function activate(context: vscode.ExtensionContext) {
         const baseUri = panel.webview.asWebviewUri(vscode.Uri.joinPath(workspaceFolders[0].uri, 'folder_name'));
         const htmlContent = `
             <!DOCTYPE html>
-            <html lang="en">
+            <html>
             <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 <title>Firefox Webview Error</title>
                 <base href="${baseUri.toString()}/">
                 <link rel="stylesheet" type="text/css" href="style.css">
             </head>
             <body>
                 <h1>Hello from Webview</h1>
-                <p>This is a sample content loaded in the webview.</p>
             </body>
             </html>
         `;
